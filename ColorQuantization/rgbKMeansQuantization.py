4bcec57989526883898d6386a31eee52f2dc9819
vicorhe <vctr.he@gmail.com>
2018-12-14 00:47:21 +0800

    RGB K Means Attempt

---
+++ ColorQuantization/rgbKMeansQuantization.py
@@ -0,0 +1,147 @@
+# import the necessary packages
+from sklearn.cluster import MiniBatchKMeans
+from sklearn.datasets import load_sample_image
+import numpy as np
+import argparse
+import cv2
+from time import time
+
+
+
+image = load_sample_image("china.jpg")
+image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+
+# construct the argument parser and parse the arguments
+# ap = argparse.ArgumentParser()
+# ap.add_argument("-i", "--image", required=True, help="Path to the image")
+# ap.add_argument("-c", "--clusters", required=True, type=int,
+#                help="# of clusters")
+# args = vars(ap.parse_args())
+
+# load the image and grab its width and height
+#image = cv2.imread(args["image"])
+(h, w) = image.shape[:2]
+
+# convert the image from the RGB color space to the L*a*b*
+# color space -- since we will be clustering using k-means
+# which is based on the euclidean distance, we'll use the
+# L*a*b* color space where the euclidean distance implies
+# perceptual meaning
+image = cv2.cvtColor(image, cv2.COLOR_BGR2LAB)
+
+# reshape the image into a feature vector so that k-means
+# can be applied
+image = image.reshape((image.shape[0] * image.shape[1], 3))
+
+# apply k-means using the specified number of clusters and
+# then create the quantized image based on the predictions
+clt = MiniBatchKMeans(n_clusters=256)#args["clusters"])
+
+ti = time()
+labels = clt.fit_predict(image)
+quant = clt.cluster_centers_.astype("uint8")[labels]
+print("done in %0.3fs." % (time() - ti))
+
+
+# reshape the feature vectors to images
+quant = quant.reshape((h, w, 3))
+image = image.reshape((h, w, 3))
+
+# convert from L*a*b* to RGB
+quant = cv2.cvtColor(quant, cv2.COLOR_LAB2BGR)
+image = cv2.cvtColor(image, cv2.COLOR_LAB2BGR)
+
+# display the images and wait for a keypress
+cv2.imshow("image", np.hstack([image, quant]))
+cv2.waitKey(0)
+
+
+
+
+
+
+
+
+
+'''
+import numpy as np
+from matplotlib import pyplot as plt
+from sklearn.cluster import KMeans
+from sklearn.metrics import pairwise_distances_argmin
+from sklearn.datasets import load_sample_image
+from sklearn.utils import shuffle
+from time import time
+
+n_colors = 64
+
+# Load the Summer Palace photo
+china = load_sample_image("china.jpg")
+
+# Convert to floats instead of the default 8 bits integer coding. Dividing by
+# 255 is important so that plt.imshow behaves works well on float data (need to
+# be in the range [0-1])
+china = np.array(china, dtype=np.float64) / 255
+
+# Load Image and transform to a 2D numpy array.
+w, h, d = original_shape = tuple(china.shape)
+print('size', (w, h, d))
+assert d == 3
+image_array = np.reshape(china, (w * h, d))
+
+print("Fitting model on a small sub-sample of the data")
+t0 = time()
+image_array_sample = shuffle(image_array, random_state=0)[:1000]
+kmeans = KMeans(n_clusters=n_colors, random_state=0).fit(image_array_sample)
+print("done in %0.3fs." % (time() - t0))
+
+# Get labels for all points
+print("Predicting color indices on the full image (k-means)")
+t0 = time()
+labels = kmeans.predict(image_array)
+print("done in %0.3fs." % (time() - t0))
+
+codebook_random = shuffle(image_array, random_state=0)[:n_colors]
+
+print("Predicting color indices on the full image (random)")
+t0 = time()
+labels_random = pairwise_distances_argmin(codebook_random,
+                                          image_array,
+                                          axis=0)
+print("done in %0.3fs." % (time() - t0))
+
+
+def recreate_image(codebook, labels, w, h):
+    t_0 = time()
+    """Recreate the (compressed) image from the code book & labels"""
+    d = codebook.shape[1]
+    image = np.zeros((w, h, d))
+    label_idx = 0
+    for i in range(w):
+        for j in range(h):
+            image[i][j] = codebook[labels[label_idx]]
+            label_idx += 1
+    print("recreating image done in %0.3fs." % (time() - t_0))
+
+    return image
+
+# Display all results, alongside original image
+plt.figure(1)
+plt.clf()
+plt.axis('off')
+plt.title('Original image (96,615 colors)')
+plt.imshow(china)
+
+plt.figure(2)
+plt.clf()
+plt.axis('off')
+plt.title('Quantized image (256 colors, K-Means)')
+plt.imshow(recreate_image(kmeans.cluster_centers_, labels, w, h))
+
+plt.figure(3)
+plt.clf()
+plt.axis('off')
+plt.title('Quantized image (256 colors, Random)')
+plt.imshow(recreate_image(codebook_random, labels_random, w, h))
+plt.show()
+'''
