bd1d8aff39ccf95ee713e5cd25875b53233421a0
vicorhe <vctr.he@gmail.com>
2018-12-19 20:46:18 +0800

    ycrcb histogram

--- ColorQuantization/ycrcbHistogram.py
+++ ColorQuantization/ycrcbHistogram.py
@@ -12,7 +12,7 @@ def get_image_set():
     image_set = list()
     for path in sorted(glob('/Users/victorhe/Pictures/colorQuantization/%s/*.jpeg' % set_param)):
         img = cv.imread(path)
-        img = cv.cvtColor(img, cv.COLOR_BGR2LAB)
+        img = cv.cvtColor(img, cv.COLOR_BGR2YCrCb)
         image_set.append((img, path.split('/')[-1]))
     return image_set
 
@@ -20,19 +20,19 @@ def get_image_set():
 def plot_image_set(image_set):
     set_size = len(image_set)
     for i, (image, name) in enumerate(image_set):
-        rgb = cv.cvtColor(image, cv.COLOR_LAB2RGB)
+        rgb = cv.cvtColor(image, cv.COLOR_YCrCb2RGB)
 
         plt.subplot(set_size, 2, 2*i+1), plt.imshow(rgb)
         plt.title(name), plt.xticks([]), plt.yticks([])
 
-        plt.subplot(set_size, 2, 2*i+2), plot_lab_histogram(image)
+        plt.subplot(set_size, 2, 2*i+2), plot_ycrcb_histogram(image)
         plt.title('hist_' + str(i + 1)), plt.xticks([]), plt.yticks([])
 
     plt.show()
 
 
-def plot_lab_histogram(image):
-    color = ('k', 'm', 'y')
+def plot_ycrcb_histogram(image):
+    color = ('k', 'c', 'y')
     for i, col in enumerate(color):
         hist = cv.calcHist([image], [i], None, [256], [0, 256])
         plt.plot(hist, color=col)
