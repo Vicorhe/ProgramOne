c2f6cb3ffb21934e4f0ea2d914c02fbc2a9d8a40
vicorhe <vctr.he@gmail.com>
2018-12-19 20:16:16 +0800

    added boiler plate code

---
+++ ColorQuantization/ycrcbHistogram.py
@@ -0,0 +1,42 @@
+import argparse
+from glob import glob
+import cv2 as cv
+from matplotlib import pyplot as plt
+
+
+def get_image_set():
+    ap = argparse.ArgumentParser()
+    ap.add_argument('-s', '--set', required=True, help='set of images being evaluated')
+    args = vars(ap.parse_args())
+    set_param = args['set']
+    image_set = list()
+    for path in sorted(glob('/Users/victorhe/Pictures/colorQuantization/%s/*.jpeg' % set_param)):
+        img = cv.imread(path)
+        hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+        image_set.append((hsv, path.split('/')[-1]))
+    return image_set
+
+
+def plot_image_set(image_set):
+    set_size = len(image_set)
+    for i, (image, name) in enumerate(image_set):
+        rgb = cv.cvtColor(image, cv.COLOR_HSV2RGB)
+        plt.subplot(set_size, 2, 2*i+1), plt.imshow(rgb)
+        plt.title(name), plt.xticks([]), plt.yticks([])
+
+        plt.subplot(set_size, 2, 2*i+2), plot_hsv_histogram(image)
+        plt.title('hist_' + str(i + 1)), plt.xticks([]), plt.yticks([])
+    plt.show()
+
+
+def plot_hsv_histogram(image):
+    hist_hue = cv.calcHist([image], [0], None, [180], [0, 180])
+    hist_saturation = cv.calcHist([image], [1], None, [256], [0, 256])
+    hist_hue_value = cv.calcHist([image], [2], None, [256], [0, 256])
+    plt.plot(hist_hue, color='y')
+    plt.plot(hist_saturation, color='b')
+    plt.plot(hist_hue_value, color='g')
+    plt.xlim([0, 256])
+
+
+plot_image_set(get_image_set())
