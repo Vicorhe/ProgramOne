841a4dbdb645f38954dc4386ade2430ec313cc3a
vicorhe <vctr.he@gmail.com>
2018-12-25 23:04:11 +0800

    image vertical concatenation utility complete

--- Utilities/concatenation.py
+++ Utilities/concatenation.py
@@ -6,15 +6,15 @@ import numpy as np
 
 ap = argparse.ArgumentParser()
 ap.add_argument('-s', '--set', required=True, help='set of images being evaluated')
-ap.add_argument('-n', '--name', required=True, help='name of output file')
 args = vars(ap.parse_args())
 image_set = list()
+
 for path in sorted(glob('/Users/victorhe/Pictures/colorQuantization/%s/*.jpeg' % args['set'])):
     img = cv.imread(path)
     image_set.append(img)
 
 result_image = np.concatenate(image_set)
 
-filename = '/Users/victorhe/Pictures/colorQuantization/%s/%s_color_range.jpeg' % (args['set'], args['name'])
-print(filename)
-#cv.imwrite(filename, result_image)
+filename = '/Users/victorhe/Pictures/colorQuantization/%s/%s_color_space.jpeg' % (args['set'], args['set'])
+
+cv.imwrite(filename, result_image)
