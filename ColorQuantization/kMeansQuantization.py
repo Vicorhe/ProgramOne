8e81560a06011a2d8cdd57b33c510b591afbc047
vicorhe <vctr.he@gmail.com>
2018-12-26 23:28:15 +0800

    clean up KMeans

--- ColorQuantization/kMeansQuantization.py
+++ ColorQuantization/kMeansQuantization.py
@@ -102,65 +102,8 @@ for i, img_path in enumerate(image_set):
 #   plt.legend(bbox_to_anchor=(0., 1.02, 1., .202), loc=3, ncol=2, mode="expand", borderaxespad=0.)
 plt.show()
 
-# dump model
+#  saves models
 joblib.dump(model, model_pkg_name)
 
-
-def quantize(raster, n_colors):
-
-    width, height, depth = raster.shape
-    reshaped_raster = np.reshape(raster, (width * height, depth))
-
-    t_1 = time()
-    model = KMeans(n_clusters=n_colors)
-    model.fit(reshaped_raster)
-    print("kmeans training done in %0.3fs." % (time() - t_1))
-
-    t_2 = time()
-    model_m = MiniBatchKMeans(n_clusters=n_colors)
-    model_m.fit(reshaped_raster)
-    print("mini batch kmeans training done in %0.3fs." % (time() - t_2))
-
-    t_3 = time()
-    labels = model.predict(reshaped_raster)
-    print("kmeans predict mapping done in %0.3fs." % (time() - t_3))
-
-    t_4 = time()
-    labels_m = model_m.predict(reshaped_raster)
-    print("mini batch kmeans predict mapping done in %0.3fs." % (time() - t_4))
-
-    palette = model.cluster_centers_
-    palette_m = model_m.cluster_centers_
-
-    t_5 = time()
-    labels_ = pairwise_distances_argmin(reshaped_raster, palette)
-    print("pairwise kmeans mapping done in %0.3fs." % (time() - t_5))
-
-    t_6 = time()
-    labels_m_ = pairwise_distances_argmin(reshaped_raster, palette_m)
-    print("pairwise mini batch kmeans mapping done in %0.3fs." % (time() - t_6))
-
-    print(all(labels_ == labels))
-    print(all(labels_m == labels_m_))
-
-    t_7 = time()
-    quantized_raster = np.reshape(palette[labels], (width, height, palette.shape[1]))
-    quantized_raster = np.reshape(palette_m[labels_m], (width, height, palette_m.shape[1]))
-    print("done in %0.3fs." % (time() - t_7))
-
-    return quantized_raster
-
-
-#raster = scipy.misc.imread(image_filename)
-
-#lab_raster = color.rgb2lab(raster)
-
-#q_lab = quantize(lab_raster, n)
-
-#q_lab_as_rgb = (color.lab2rgb(q_lab) * 255).astype('uint8')
-
-#plt.imshow(q_lab_as_rgb)
-#plt.draw()
-#plt.show()
-cv.waitKey(0)
-cv.destroyAllWindows()
+#  cv.waitKey(0)
+#  cv.destroyAllWindows()
