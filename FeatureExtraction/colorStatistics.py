9137677afda74fd98371a49646384af166f87aae
vicorhe <vctr.he@gmail.com>
2019-01-10 16:17:08 +0800

    color statistics image aquaisition mechanics

--- FeatureExtraction/colorStatistics.py
+++ FeatureExtraction/colorStatistics.py
@@ -3,38 +3,38 @@ from glob import glob
 import cv2 as cv
 from matplotlib import pyplot as plt
 
-options = {'rgb': (cv.COLOR_BGR2RGB, ),
-           'hsv': (cv.COLOR_BGR2HSV, ),
-           'lab': (cv.COLOR_BGR2LAB, ),
-           'ycrcb': (cv.COLOR_BGR2YCR_CB, ),
-           'gray': (cv.COLOR_BGR2GRAY, 1)}
+options = {'rgb': (cv.COLOR_BGR2RGB, ('red', 'green', 'blue')),
+           'hsv': (cv.COLOR_BGR2HSV, ('hue', 'saturation', 'value')),
+           'lab': (cv.COLOR_BGR2LAB, ('luminance', 'a component', 'b component')),
+           'ycrcb': (cv.COLOR_BGR2YCR_CB, ('y component', 'Cr', 'Cb')),
+           'gray': (cv.COLOR_BGR2GRAY, ('value'))}
 
 ap = argparse.ArgumentParser()
 ap.add_argument('-o', '--option', required=True, help='color space option to evaluate, choices: rgb, hsv, lab, ycrcb, gray')
 ap.add_argument('-s', '--set', required=True, help='set of images being evaluated')
 args = vars(ap.parse_args())
+
 set_param = args['set']
+color_space, dimensions = options[args['option']]
+
+print(color_space)
+
 image_set = list()
 
+# get all color space converted images and their corresponding labels
 for path in sorted(glob('/Users/victorhe/Pictures/colorQuantization/%s/*' % set_param)):
-    image_set.append((cv.imread(path, 0), path.split('/')[-1]))
+    image_label = path.split('/')[-1].split('.')[0]
+    image = cv.cvtColor(cv.imread(path), color_space)
+    image_set.append((image, image_label))
 
+print(image_set)
 
-
-
-def plot_image_set(image_set):
-    set_size = len(image_set)
-    for i, (image, name) in enumerate(image_set):
-        plt.subplot(set_size, 2, 2*i+1), plt.imshow(image, cmap='gray')
-        plt.title(name), plt.xticks([]), plt.yticks([])
-
-        plt.subplot(set_size, 2, 2*i+2), plot_histogram(image)
-        plt.title('hist_' + str(i + 1)), plt.xticks([]), plt.yticks([])
-
-    plt.show()
-
+#def get_statistics(hist):
+#    return
 
 def plot_histogram(image):
     hist = cv.calcHist([image], [0], None, [256], [0, 256])
     plt.plot(hist)
     plt.xlim([0, 256])
+
+
