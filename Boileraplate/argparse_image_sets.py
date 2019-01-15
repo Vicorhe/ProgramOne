b9dc2dd1fbf62092f433e546338e5d3a8f1805e3
vicorhe <vctr.he@gmail.com>
2019-01-15 17:29:49 +0800

    refactor project structure and create boiler plate folder

---
+++ Boileraplate/argparse_image_sets.py
@@ -0,0 +1,19 @@
+import argparse
+from glob import glob
+import cv2 as cv
+import numpy as np
+
+
+ap = argparse.ArgumentParser()
+ap.add_argument('-s', '--set', required=True, help='set of images being evaluated')
+args = vars(ap.parse_args())
+
+set_param = args['set']
+
+image_set = list()
+
+# get all color space converted images and their corresponding labels
+for path in sorted(glob('/Users/victorhe/Pictures/colorQuantization/%s/*.BMP' % set_param)):
+    image_label = path.split('/')[-1].split('.')[0]
+    image = cv.imread(path)
+    image_set.append((image, image_label))
