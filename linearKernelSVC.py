667d2489eb559201c2cfab61bbe4e0857414c553
Victor He <vctr.he@gmail.com>
2019-02-20 07:25:50 +0800

    linear kernel svc implementation

--- linearKernelSVC.py
+++ linearKernelSVC.py
@@ -23,9 +23,22 @@ assert y_train.flags['C_CONTIGUOUS']
 assert y_test.flags['C_CONTIGUOUS']
 
 # linear kernel support vector classifier
+
+kernel = 'linear'
+C = 1
+shrinking = True
+tol = 1e-3
+max_iter = -1
+
 linear_kernel_SVC = Pipeline([
-    ('linear_kernel_svc', SVC(kernel='linear', C=1, gamma='scale'))
+    ('linear_kernel_svc', SVC(kernel=kernel, C=C, shrinking=shrinking, tol=tol, max_iter=max_iter))
 ])
+
+#   C = 0.001, 0.01, 0.1, 1, 10, 100
+#   shrinking = True, False
+#   tol = 1e-2, 1e-3, 1e-4
+#   max_iter = -1, 1000, 2000, 3000, 4000
+
 
 linear_kernel_SVC.fit(X_train, y_train)
 
