8710649b88344b0960f21d088ce192c3ca2a5eb2
Victor He <vctr.he@gmail.com>
2019-02-21 08:58:54 +0800

    pasting implementation

---
+++ pasting.py
@@ -0,0 +1,48 @@
+import numpy as np
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler, Normalizer
+from sklearn.ensemble import BaggingClassifier
+from sklearn.tree import DecisionTreeClassifier
+from Miscellaneous.imageAcquisition import get_data_set
+# from FeatureExtraction.feature_set_b import get_statistics
+from FeatureExtraction.feature_set_a import get_statistics
+from Utilities.utils import unison_shuffled_copies
+
+
+# fetch the raw training and testing images, and respective labels
+training_images, testing_images, y_train, y_test, channels = get_data_set()
+
+# gather features
+X_train = np.vstack([get_statistics(img, channels) for img, _ in training_images])
+X_test = np.vstack([get_statistics(img, channels) for img, _ in testing_images])
+X_train, y_train = unison_shuffled_copies(X_train, y_train)
+
+# avoid data copy
+assert X_train.flags['C_CONTIGUOUS']
+assert X_test.flags['C_CONTIGUOUS']
+assert y_train.flags['C_CONTIGUOUS']
+assert y_test.flags['C_CONTIGUOUS']
+
+# pasting classifier
+
+base_estimator = DecisionTreeClassifier()
+n_estimators = 10
+max_samples = 0.25
+bootstrap = False
+n_jobs = -1
+
+pasting_clf = Pipeline([
+    ('pasting_clf', BaggingClassifier(base_estimator=base_estimator,
+                                      n_estimators=n_estimators,
+                                      max_samples=max_samples,
+                                      bootstrap=bootstrap,
+                                      n_jobs=n_jobs))
+])
+#   base_estimator = DecisionTreeClassifier(), SVC(), KNeighborsClassifier()
+#   n_estimators = 10, 50, 100, 300, 500
+#   max_samples = 0.25, 0.5, 0.67, 0.8
+
+pasting_clf.fit(X_train, y_train)
+
+print("training score: %f" % (pasting_clf.score(X_train, y_train)))
+print("testing score: %f" % (pasting_clf.score(X_test, y_test)))
