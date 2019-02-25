0f0b717dcde5ab45e97fb3fba6c36207a7ed51b0
Victor He <vctr.he@gmail.com>
2019-02-25 19:59:57 +0800

    formated feature importance util output

--- featureImportance.py
+++ featureImportance.py
@@ -1,26 +1,12 @@
-import numpy as np
-from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import StandardScaler, Normalizer
 from sklearn.ensemble import RandomForestClassifier
-from Datasets.tiles import get_raw_data_set
-from FeatureExtraction.feature_set_b import get_statistics, get_feature_names
+from Datasets.utils import load_tile_data_set
 # from FeatureExtraction.feature_set_a import get_statistics, get_feature_names
-from Utilities.utils import unison_shuffled_copies
+from FeatureExtraction.feature_set_b import get_statistics, get_feature_names
 
 
-# fetch the raw training and testing images, and respective labels
-training_images, testing_images, y_train, y_test, channels = get_raw_data_set()
+# load data set
+X_train, y_train, X_test, y_test = load_tile_data_set(feature_func=get_statistics)
 
-# gather features
-X_train = np.vstack([get_statistics(img, channels) for img, _ in training_images])
-X_test = np.vstack([get_statistics(img, channels) for img, _ in testing_images])
-X_train, y_train = unison_shuffled_copies(X_train, y_train)
-
-# avoid data copy
-assert X_train.flags['C_CONTIGUOUS']
-assert X_test.flags['C_CONTIGUOUS']
-assert y_train.flags['C_CONTIGUOUS']
-assert y_test.flags['C_CONTIGUOUS']
 
 # random forest ensemble classifier
 
@@ -31,11 +17,11 @@ n_jobs = -1
 random_forest_clf = RandomForestClassifier(n_estimators=n_estimators,
                                            max_leaf_nodes=max_leaf_nodes,
                                            n_jobs=n_jobs)
-
 random_forest_clf.fit(X_train, y_train)
 
-print("training score: %f" % (random_forest_clf.score(X_train, y_train)))
-print("testing score: %f" % (random_forest_clf.score(X_test, y_test)))
 
-for name, score in zip(get_feature_names(), random_forest_clf.feature_importances_):
-    print(name, score)
+feature_importance = zip(get_feature_names(),
+                         random_forest_clf.feature_importances_)
+
+for name, score in sorted(feature_importance, key=lambda x : x[1], reverse=True):
+    print('{:>11s}{:10.4f}'.format(name, score))
