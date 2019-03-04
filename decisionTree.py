6c3cf3bba7c845c7f5800fd86b6ef9b6e5664451
Victor He <vctr.he@gmail.com>
2019-03-04 17:58:21 +0800

    decision tree and logistic SGD implementation

---
+++ decisionTree.py
@@ -0,0 +1,35 @@
+from sklearn.pipeline import Pipeline
+from sklearn.tree import DecisionTreeClassifier
+from Datasets.utils import load_tile_data_set
+from FeatureExtraction.feature_set_a import get_statistics
+# from FeatureExtraction.feature_set_b import get_statistics
+from Evaluation.crossValidation import cross_validation_report
+
+
+# load data set
+X_train, y_train, X_test, y_test = load_tile_data_set(feature_func=get_statistics)
+
+
+# decision tree classifier
+
+criterion = 'gini'
+max_depth = 3
+min_samples_leaf = 4
+random_state = 6117
+presort = True
+
+decision_tree_clf = Pipeline([
+    ('decision_tree_clf', DecisionTreeClassifier(criterion=criterion,
+                                                 max_depth=max_depth,
+                                                 min_samples_leaf=min_samples_leaf,
+                                                 random_state=random_state,
+                                                 presort=presort))
+]).fit(X_train, y_train)
+#   criterion = 'gini', entropy'
+#   max_depth = 3, 4, 5
+#   min_samples_leaf = 2, 3, 4
+
+
+# cross validation
+cross_validation_report(decision_tree_clf, X_train, y_train)
+
---
+++ logisticSGD.py
@@ -0,0 +1,33 @@
+from sklearn.pipeline import Pipeline
+from sklearn.linear_model import SGDClassifier
+from Datasets.utils import load_tile_data_set
+from FeatureExtraction.feature_set_a import get_statistics
+# from FeatureExtraction.feature_set_b import get_statistics
+from Evaluation.crossValidation import cross_validation_report
+
+
+# load data set
+X_train, y_train, X_test, y_test = load_tile_data_set(feature_func=get_statistics)
+
+
+# log loss stochastic gradient descent classifier
+
+loss = 'log'
+penalty = 'l2'
+max_iter = 1000
+n_jobs = -1
+random_state = 6117
+
+
+SGD_clf = Pipeline([
+    ('SGD', SGDClassifier(loss=loss, penalty=penalty, max_iter=max_iter,
+                          n_jobs=n_jobs, random_state=random_state))
+]).fit(X_train, y_train)
+#   loss = 'hinge', 'log', 'modified_huber'
+#   penalty = 'l1', 'l2', 'elasticnet'
+#   max_iter = 1000, 2000, 3000, 4000, 5000
+
+
+# cross validation
+cross_validation_report(SGD_clf, X_train, y_train)
+
