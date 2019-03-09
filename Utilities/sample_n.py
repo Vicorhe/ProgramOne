5dda5e255938a01e3f98b6df0e55fdf36f7fe3e5
Victor He <vctr.he@gmail.com>
2019-03-09 22:52:37 +0800

    create 4 quandrant version of sample n utility

--- Utilities/sample_n.py
+++ Utilities/sample_n.py
@@ -3,35 +3,39 @@ from random import randint
 import cv2 as cv
 
 
-DIMENSION = 150
+STUDIO_SET = 'SS7'
+NUM_SAMPLES = 30
+DIMENSION = 50
 GRANULARITY = 30
-RANDOM_LIMIT = (1000 - DIMENSION) // GRANULARITY
+RANDOM_LIMIT = (500 - DIMENSION) // GRANULARITY
 
-source_path = '/Users/victorhe/Pictures/studioSourceTiles/SS5/cropped/%s.BMP'
-destination_path = '/Users/victorhe/Pictures/studioSourceTiles/SS5/%s/%s_%d.BMP'
+source_path = '/Users/victorhe/Pictures/studioSourceTiles/%s/cropped/%s.BMP'
+
+destination_path = '/Users/victorhe/Pictures/studioSourceTiles/%s/%s/%s/%s_%d.BMP'
 
 ap = argparse.ArgumentParser()
 ap.add_argument('-s', '--set', required=True,
                 help='set of images being evaluated')
 ap.add_argument('-i', '--image', required=True,
                 help='image to be split')
-ap.add_argument('-n', '--number', default=200, type=int,
-                help='number of samples to generate')
 args = vars(ap.parse_args())
 image_set = list()
 
-source_path = source_path % (args['image'])
-print(source_path)
+source_path = source_path % (STUDIO_SET, args['image'])
 
-unique_samples = set()
 img = cv.imread(source_path)
+img_quadrants = ((img[:500, :500], 'W'),
+                 (img[:500, 500:], 'X'),
+                 (img[500:, :500], 'Y'),
+                 (img[500:, 500:], 'Z'))
 
-
-for i in range(args['number']):
-    tl_x = randint(0, RANDOM_LIMIT) * GRANULARITY
-    tl_y = randint(0, RANDOM_LIMIT) * GRANULARITY
-    unique_samples.add((tl_x, tl_y))
-    destination = destination_path % (args['set'], args['set'], i)
-    cv.imwrite(destination, img[tl_x: tl_x + DIMENSION, tl_y: tl_y + DIMENSION])
-
-print('uniqueness ratio: %.3f' % (len(unique_samples)/args['number']))
+for quadrant, folder in img_quadrants:
+    unique_samples = set()
+    for i in range(NUM_SAMPLES):
+        tl_x = randint(0, RANDOM_LIMIT) * GRANULARITY
+        tl_y = randint(0, RANDOM_LIMIT) * GRANULARITY
+        unique_samples.add((tl_x, tl_y))
+        sample_name = args['set'] + '_' + folder
+        destination = destination_path % (STUDIO_SET, args['set'], folder, sample_name, i)
+        cv.imwrite(destination, img[tl_x: tl_x + DIMENSION, tl_y: tl_y + DIMENSION])
+    print('uniqueness ratio: %.3f' % (len(unique_samples)/NUM_SAMPLES))
