84287a76a006d4b86dedaa0f3102a823423a7cb2
Victor He <vctr.he@gmail.com>
2019-03-11 23:49:19 +0800

    load tile data set was revamped to support manual spliting

--- DATASETOPS.py
+++ DATASETOPS.py
@@ -13,87 +13,100 @@ COLOR_SPACE_OPTIONS = {'rgb': (cv.COLOR_BGR2RGB, ('R', 'G', 'B')),
                        'ycrcb': (cv.COLOR_BGR2YCR_CB, ('Y', 'Cr', 'Cb'))}
 FEATURE_SPACE_OPTIONS = {'a': feature_set_a, 'b': feature_set_b}
 QUADRANTS = ['W', 'X', 'Y', 'Z']
-
 SET_PATH = '/Users/victorhe/Pictures/tileDataSet/%s/*.BMP'
-CLASSIC_LABELS_PATH = '/Users/victorhe/Pictures/tileDataSet/%s/labels.txt'
-
-
-
-ap = argparse.ArgumentParser()
-ap.add_argument('-s', '--set',
-                required=True,
-                help='set of images being evaluated')
-ap.add_argument('-f', '--feature_set',
-                default='a',
-                help='which feature set to use',
-                choices=['a', 'b'])
-ap.add_argument('-c', '--color_space',
-                default='hsv',
-                help='color space option to evaluate',
-                choices=['rgb', 'hsv', 'lab', 'ycrcb'])
-ap.add_argument('-m', '--manual',
-                help='indicates to perform quadrant based splitting',
-                action='store_true')
-args = vars(ap.parse_args())
-
-
-img_set = args['set']
-feature_function = FEATURE_SPACE_OPTIONS[args['feature_set']]
-color_space, channels = COLOR_SPACE_OPTIONS[args['color_space']]
-manual = args['manual']
-
-image_set, test_image_set, y, test_y = list(), list(), list(), list()
-
-
-set_of_paths = sorted(glob(SET_PATH % img_set))
-
-if manual:
-    for test_quadrant in QUADRANTS:
-        test_paths = [x for x in set_of_paths if test_quadrant in x]
-        train_paths = [x for x in set_of_paths if test_quadrant not in x]
-
-
-        CONTINUE HERE!!!!!!!!
-
-
-else:
-    # classic image round up
-    for path in set_of_paths:
-        image_label = path.split('/')[-1].split('.')[0]
-        converted_image = cv.cvtColor(cv.imread(path), color_space)
-        image_set.append((converted_image, image_label))
-
-    with open(CLASSIC_LABELS_PATH % img_set) as f:
-        for line in f:
-            y = np.array(line.split())
-
+TRAIN_LABELS_PATH = '/Users/victorhe/Pictures/tileDataSet/%s/train_labels.txt'
+TEST_LABELS_PATH = '/Users/victorhe/Pictures/tileDataSet/%s/test_labels.txt'
+LABELS_PATH = '/Users/victorhe/Pictures/tileDataSet/%s/labels.txt'
 
 
 def load_tile_data_set():
+    ap = argparse.ArgumentParser()
+    ap.add_argument('-s', '--set',
+                    required=True,
+                    help='set of images being evaluated')
+    ap.add_argument('-f', '--feature_set',
+                    default='a',
+                    help='which feature set to use',
+                    choices=['a', 'b'])
+    ap.add_argument('-c', '--color_space',
+                    default='hsv',
+                    help='color space option to evaluate',
+                    choices=['rgb', 'hsv', 'lab', 'ycrcb'])
+    ap.add_argument('-m', '--manual',
+                    help='indicates to perform quadrant based splitting',
+                    action='store_true')
+    args = vars(ap.parse_args())
+    img_set = args['set']
+    feature_function = FEATURE_SPACE_OPTIONS[args['feature_set']]
+    color_space, channels = COLOR_SPACE_OPTIONS[args['color_space']]
+    manual = args['manual']
+    set_of_paths = sorted(glob(SET_PATH % img_set))
 
-    if not manual:
-        # generate feature matrix from image set
-        X = np.vstack([feature_function(img, channels) for img, _ in image_set])
+    training_data, testing_data, training_labels, testing_labels = list(), list(), None, None
+
+    if manual:
+        for quadrant in QUADRANTS:
+
+            train_data_container, test_data_container = list(), list()
+
+            train_paths = [x for x in set_of_paths if quadrant not in x]
+            test_paths = [x for x in set_of_paths if quadrant in x]
+
+            # build the training data matrix
+            for train_path in train_paths:
+                train_image = cv.imread(train_path)
+                converted_train_image = cv.cvtColor(train_image, color_space)
+                train_data_container.append(feature_function(converted_train_image, channels))
+            training_data.append(np.vstack(train_data_container))
+
+            # build the testing data matrix
+            for test_path in test_paths:
+                test_image = cv.imread(test_path)
+                converted_test_image = cv.cvtColor(test_image, color_space)
+                test_data_container.append(feature_function(converted_test_image, channels))
+            testing_data.append(np.vstack(test_data_container))
+
+        training_data = np.array(training_data)
+        testing_data = np.array(testing_data)
+
+        # get training labels
+        with open(TRAIN_LABELS_PATH % img_set) as train_labels_file:
+            for line in train_labels_file:
+                training_labels = np.array(line.split())
+
+        # get testing labels
+        with open(TEST_LABELS_PATH % img_set) as test_labels_file:
+            for line in test_labels_file:
+                testing_labels = np.array(line.split())
+
+    else:
+        data_container = list()
+        for path in set_of_paths:
+            image = cv.imread(path)
+            converted_image = cv.cvtColor(image, color_space)
+            data_container.append(feature_function(converted_image, channels))
+        stacked_data = np.vstack(data_container)
+
+        labels = None
+        with open(LABELS_PATH % img_set) as labels_file:
+            for line in labels_file:
+                labels = np.array(line.split())
 
         # split training and testing set
         n_splits = 1
-        test_set_size = 0.3
+        test_set_size = 0.25
         random_state = 6117
-        sss = StratifiedShuffleSplit(n_splits=n_splits, test_size=test_set_size, random_state=random_state)
-        train_index, test_index = next(sss.split(X, y))
-        X_train, y_train = X[train_index], y[train_index]
-        X_test, y_test = X[test_index], y[test_index]
+        sss = StratifiedShuffleSplit(n_splits=n_splits, test_size=test_set_size,
+                                     random_state=random_state)
+        train_index, test_index = next(sss.split(stacked_data, labels))
 
-    else:
-        X_train = np.vstack([feature_function(img, channels) for img, _ in image_set])
-        y_train = y
-        X_test = np.vstack([feature_function(img, channels) for img, _ in test_image_set])
-        y_test = test_y
+        training_data, training_labels = np.array([stacked_data[train_index]]), labels[train_index]
+        testing_data, testing_labels = np.array([stacked_data[test_index]]), labels[test_index]
 
     # avoid data copy
-    assert X_train.flags['C_CONTIGUOUS']
-    assert X_test.flags['C_CONTIGUOUS']
-    assert y_train.flags['C_CONTIGUOUS']
-    assert y_test.flags['C_CONTIGUOUS']
+    assert training_data.flags['C_CONTIGUOUS']
+    assert testing_data.flags['C_CONTIGUOUS']
+    assert training_labels.flags['C_CONTIGUOUS']
+    assert testing_labels.flags['C_CONTIGUOUS']
 
-    return X_train, y_train, X_test, y_test
+    return training_data, testing_data, training_labels, testing_labels
--- Utilities/labels_generator.py
+++ Utilities/labels_generator.py
@@ -1,16 +1,18 @@
 import argparse
 
 
-OUTPUT_PATH = '/Users/victorhe/Pictures/tileDataSet/labels.txt'
+OUTPUT_PATH = '/Users/victorhe/Pictures/tileDataSet/%s.txt'
 
 ap = argparse.ArgumentParser()
 ap.add_argument('-c', '--classes', required=True, type=int,
                 help='the number of distinct classes')
 ap.add_argument('-i', '--instances', required=True, type=int,
                 help='the number of instances per classes')
+ap.add_argument('-n', '--name', default='labels',
+                help='name of the labels file')
 args = vars(ap.parse_args())
 
-with open(OUTPUT_PATH, "w") as text_file:
+with open(OUTPUT_PATH % args['name'], "w") as text_file:
     for i in range(args['classes']):
         s = (str(i) + ' ') * args['instances']
         text_file.write(s)
