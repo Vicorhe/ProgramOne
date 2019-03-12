a7d2da87d430951907fda5f36e96adbdbe839251
Victor He <vctr.he@gmail.com>
2019-03-12 15:56:31 +0800

    updated cross validation to accomidate new training set structures

--- Evaluation/crossValidation.py
+++ Evaluation/crossValidation.py
@@ -6,30 +6,30 @@ CENTER_TEXT = '{:^22s}'
 TABLE_HEADER = '{:>11s}{:>11s}'
 TABLE_ROW = '{:>11.3f}{:>11.3f}'
 TABLE_BORDER = '-' * 22
+TABLE_OUTER_BORDER = '*' * 22
 
 
-def cross_validation_report(clf, data, labels):
-    cv = 4
+def print_report(f1_macro, score):
+    print(TABLE_OUTER_BORDER)
+    print(CENTER_TEXT.format('CV REPORT'))
+    print(TABLE_BORDER)
+    print(TABLE_HEADER.format('f1 macro', 'accuracy'))
+    print(TABLE_BORDER)
+    print(TABLE_ROW.format(f1_macro, score))
+    print(TABLE_OUTER_BORDER)
+
+
+def cross_validation_report(clf, feature_sets, labels):
+    cv = 3
     scoring = ['f1_macro', 'f1_micro', 'accuracy']
     n_jobs = -1
     return_train_score = False
-    scores = cross_validate(clf, data, labels,
-                            scoring=scoring, cv=cv, n_jobs=n_jobs,
-                            return_train_score=return_train_score)
-    f1_macro = np.mean(scores['test_f1_macro'])
-    f1_micro = np.mean(scores['test_f1_micro'])
-    score = np.mean(scores['test_accuracy'])
 
-    print(TABLE_BORDER)
+    for feature_set in feature_sets:
+        scores = cross_validate(clf, feature_set, labels,
+                                scoring=scoring, cv=cv, n_jobs=n_jobs,
+                                return_train_score=return_train_score)
+        f1_macro = np.mean(scores['test_f1_macro'])
+        score = np.mean(scores['test_accuracy'])
 
-    print(CENTER_TEXT.format('CV REPORT'))
-
-    print(TABLE_BORDER)
-
-    print(TABLE_HEADER.format('f1 macro', 'accuracy'))
-
-    print(TABLE_BORDER)
-
-    print(TABLE_ROW.format(f1_macro, score))
-
-    print(TABLE_BORDER)
+        print_report(f1_macro, score)
