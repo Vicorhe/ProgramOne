10c6dc6da21b49e6148bc5f2401ee875581db2f7
Victor He <vctr.he@gmail.com>
2019-03-12 17:58:57 +0800

    ruthlessly cut out evaluation metrics to bare minimum

--- Evaluation/crossValidation.py
+++ Evaluation/crossValidation.py
@@ -1,35 +1,19 @@
-import numpy as np
+from numpy import mean
 from sklearn.model_selection import cross_validate
 
 
-CENTER_TEXT = '{:^22s}'
-TABLE_HEADER = '{:>11s}{:>11s}'
-TABLE_ROW = '{:>11.3f}{:>11.3f}'
-TABLE_BORDER = '-' * 22
-TABLE_OUTER_BORDER = '*' * 22
-
-
-def print_report(f1_macro, score):
-    print(TABLE_OUTER_BORDER)
-    print(CENTER_TEXT.format('CV REPORT'))
-    print(TABLE_BORDER)
-    print(TABLE_HEADER.format('f1 macro', 'accuracy'))
-    print(TABLE_BORDER)
-    print(TABLE_ROW.format(f1_macro, score))
-    print(TABLE_OUTER_BORDER)
+ROW = '{:>5s}:{:>8.3f}'
 
 
 def cross_validation_report(clf, feature_sets, labels):
+    scores = list()
     cv = 3
-    scoring = ['f1_macro', 'f1_micro', 'accuracy']
+    scoring = ['accuracy']
     n_jobs = -1
     return_train_score = False
-
     for feature_set in feature_sets:
-        scores = cross_validate(clf, feature_set, labels,
-                                scoring=scoring, cv=cv, n_jobs=n_jobs,
-                                return_train_score=return_train_score)
-        f1_macro = np.mean(scores['test_f1_macro'])
-        score = np.mean(scores['test_accuracy'])
-
-        print_report(f1_macro, score)
+        c_v = cross_validate(clf, feature_set, labels, scoring=scoring, cv=cv,
+                             n_jobs=n_jobs, return_train_score=return_train_score)
+        accuracy = mean(c_v['test_accuracy'])
+        scores.append(accuracy)
+    print(ROW.format('CVAL', mean(scores)))
--- Evaluation/performance.py
+++ Evaluation/performance.py
@@ -1,52 +1,18 @@
-from sklearn.metrics import accuracy_score, classification_report
+from numpy import mean
 from sklearn.base import clone
 
 
-CENTER_TEXT = '{:^24s}'
-TABLE_HEADER = '{:>24s}'
-TABLE_ROW = '{:>10s}{:>14.3f}'
-TABLE_BORDER = '-' * 24
-TABLE_OUTER_BORDER = '*' * 24
-
-TITLE = 'PERFORMANCE REPORT'
-MACRO = 'macro avg'
-PRECISION = 'precision'
-RECALL = 'recall'
-TRAINING = 'training'
-TESTING = 'testing'
-ACCURACY = 'accuracy'
-
-
-def print_report(precision_macro, recall_macro, testing_set_accuracy, training_set_accuracy):
-    print(TABLE_OUTER_BORDER)
-    print(CENTER_TEXT.format(TITLE))
-    print(TABLE_BORDER)
-    print(TABLE_HEADER.format(MACRO))
-    print(TABLE_BORDER)
-    print(TABLE_ROW.format(PRECISION, precision_macro))
-    print(TABLE_ROW.format(RECALL, recall_macro))
-    print(TABLE_BORDER)
-    print(TABLE_HEADER.format(ACCURACY))
-    print(TABLE_BORDER)
-    print(TABLE_ROW.format(TESTING, testing_set_accuracy))
-    print(TABLE_ROW.format(TRAINING, training_set_accuracy))
-    print(TABLE_OUTER_BORDER)
+ROW = '{:>5s}:{:>8.3f}'
 
 
 def performance_report(clf, train_feature_sets, train_labels,
                        test_feature_sets, test_labels):
-
-    for train_feature_set, test_feature_set in zip(train_feature_sets,
-                                                   test_feature_sets):
+    scores = list()
+    for i, (train_set, test_set) in enumerate(zip(train_feature_sets,
+                                                  test_feature_sets)):
         clone_clf = clone(clf)
-        clone_clf.fit(train_feature_set, train_labels)
-        test_predict = clone_clf.predict(test_feature_set)
-        train_predict = clone_clf.predict(train_feature_set)
+        clone_clf.fit(train_set, train_labels)
+        accuracy = clone_clf.score(test_set, test_labels)
+        scores.append(accuracy)
+    print(ROW.format('PERF', mean(scores)))
 
-        c_r = classification_report(test_labels, test_predict, output_dict=True)
-        precision_macro = c_r[MACRO][PRECISION]
-        recall_macro = c_r[MACRO][RECALL]
-        training_set_accuracy = accuracy_score(train_labels, train_predict)
-        testing_set_accuracy = accuracy_score(test_labels, test_predict)
-
-        print_report(precision_macro, recall_macro, testing_set_accuracy, training_set_accuracy)
