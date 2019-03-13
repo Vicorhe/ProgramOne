4deca1aa9dba0c6c9cd260ba9a99edc48dcea7a5
Victor He <vctr.he@gmail.com>
2019-03-13 14:35:06 +0800

    added meta fucntionality to the evaluation module

--- Evaluation/crossValidation.py
+++ Evaluation/crossValidation.py
@@ -2,18 +2,24 @@ from numpy import mean
 from sklearn.model_selection import cross_validate
 
 
-ROW = '{:>5s}:{:>8.3f}'
+ROW = 'CV MEAN:{:>8.3f}   MAX:{:>8.3f}   MIN:{:>8.3f}'
+NUM_RUNS = 5
 
 
 def cross_validation_report(clf, feature_sets, labels):
-    scores = list()
+    meta_stats = list()
     cv = 3
     scoring = ['accuracy']
     n_jobs = -1
     return_train_score = False
-    for feature_set in feature_sets:
-        c_v = cross_validate(clf, feature_set, labels, scoring=scoring, cv=cv,
-                             n_jobs=n_jobs, return_train_score=return_train_score)
-        accuracy = mean(c_v['test_accuracy'])
-        scores.append(accuracy)
-    print(ROW.format('CVAL', mean(scores)))
+
+    for i in range(NUM_RUNS):
+        scores = list()
+        for feature_set in feature_sets:
+            c_v = cross_validate(clf, feature_set, labels, scoring=scoring, cv=cv,
+                                 n_jobs=n_jobs, return_train_score=return_train_score)
+            accuracy = mean(c_v['test_accuracy'])
+            scores.append(accuracy)
+        meta_stats.append(mean(scores))
+
+    print(ROW.format(mean(meta_stats), max(meta_stats), min(meta_stats)))
--- Evaluation/performance.py
+++ Evaluation/performance.py
@@ -2,17 +2,21 @@ from numpy import mean
 from sklearn.base import clone
 
 
-ROW = '{:>5s}:{:>8.3f}'
+ROW = 'P  MEAN:{:>8.3f}   MAX:{:>8.3f}   MIN:{:>8.3f}'
+NUM_RUNS = 5
 
 
 def performance_report(clf, train_feature_sets, train_labels,
                        test_feature_sets, test_labels):
-    scores = list()
-    for i, (train_set, test_set) in enumerate(zip(train_feature_sets,
-                                                  test_feature_sets)):
-        clone_clf = clone(clf)
-        clone_clf.fit(train_set, train_labels)
-        accuracy = clone_clf.score(test_set, test_labels)
-        scores.append(accuracy)
-    print(ROW.format('PERF', mean(scores)))
+    meta_stats = list()
+    for i in range(NUM_RUNS):
+        scores = list()
+        for train_set, test_set in zip(train_feature_sets, test_feature_sets):
+            clone_clf = clone(clf)
+            clone_clf.fit(train_set, train_labels)
+            accuracy = clone_clf.score(test_set, test_labels)
+            scores.append(accuracy)
+        meta_stats.append(mean(scores))
+
+    print(ROW.format(mean(meta_stats), max(meta_stats), min(meta_stats)))
 
