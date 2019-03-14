b3607995e6f7abc0137eb5ed6785ddcbaa41d253
Victor He <vctr.he@gmail.com>
2019-03-14 14:53:51 +0800

    250 uniform sampler implementation

---
+++ Utilities/Sampling/uniform_medium_sample.py
@@ -0,0 +1,34 @@
+import argparse
+import cv2 as cv
+from glob import glob
+from numpy import mean
+
+
+CROPPED_SET_PATH = '/Users/victorhe/Pictures/studioSourceTiles/%s/cropped/*.BMP'
+DESTINATION_PATH = '/Users/victorhe/Pictures/studioSourceTiles/%s/%s/%s.BMP'
+NUMBER_CHUNKS = 6
+GRANULARITY = 150
+SIZE = 250
+
+
+ap = argparse.ArgumentParser()
+ap.add_argument('-s', '--set', required=True,
+                help='which studio set')
+ap.add_argument('-d', '--destination', required=True,
+                help='destination folder')
+args = vars(ap.parse_args())
+studio_set = args['set']
+destination_folder = args['destination']
+
+
+for path in sorted(glob(CROPPED_SET_PATH % studio_set)):
+    image_name = path.split('/')[-1].split('.')[0]
+    img = cv.imread(path)
+
+    for x in range(NUMBER_CHUNKS):
+        for y in range(NUMBER_CHUNKS):
+            tl_x, tl_y = x * GRANULARITY, y * GRANULARITY
+            count = str(x * NUMBER_CHUNKS + y)
+            sample_name = image_name + '_' + count
+            sample_name = DESTINATION_PATH % (studio_set, destination_folder, sample_name)
+            cv.imwrite(sample_name, img[tl_x:tl_x + SIZE, tl_y:tl_y + SIZE])
